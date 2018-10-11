7591cf6c45e1636bf88a64f05d6c646a43d7cdcf
tinyjin <baram991103@gmail.com>
2018-10-11 23:46:12 +0900

    welcome santa2d

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+	public GameObject target;
+	public float moveSpeed;
+	private Vector3 targetPosition;
+
+	public BoxCollider2D bound;
+	private Vector3 minBound;
+	private Vector3 maxBound;
+
+	private float halfWidth;
+	private float halfHeight;
+
+	private Camera theCamera;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		theCamera = GetComponent<Camera>();
+		minBound = bound.bounds.min;
+		maxBound = bound.bounds.max;
+
+		halfHeight = theCamera.orthographicSize;
+		halfWidth = halfHeight * Screen.width / Screen.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target.gameObject != null)
+		{
+			targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+
+			this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+			float clampedX = Mathf.Clamp(this.transform.position.x, minBound.x + halfWidth, maxBound.x - halfWidth);
+			float clampedY = Mathf.Clamp(this.transform.position.y, minBound.y + halfHeight, maxBound.y - halfHeight);
+			
+			this.transform.position = new Vector3(clampedX, clampedY, this.transform.position.z);
+		}
+	}
+}
---
+++ Assets/Scripts/SantaObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SantaObject : MonoBehaviour {
+	public float speed;
+	
+	private Vector3 vector;
+	private Animator animator;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+		{
+			vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+			if (vector.x != 0)
+			{
+				vector.y = 0;
+			}
+			
+			animator.SetFloat("DirX", vector.x);
+			animator.SetFloat("DirY", vector.y);
+			
+			animator.SetBool("Walking", true);
+			animator.SetBool("Standing", false);
+			
+			if (vector.x != 0)
+			{
+				transform.Translate(vector.x * speed, 0, 0);
+			}
+			else if (vector.y != 0)
+			{
+				transform.Translate(0, vector.y * speed, 0);
+			}
+		}
+		else
+		{
+			animator.SetBool("Walking", false);
+			animator.SetBool("Standing", true);
+		}
+	}
+}
